61dd7f08a6f611ae7ac84fddaee343db1a0178e2
peteg <peteg@REDDOG1>
2025-06-13 12:47:45 +0100

    Added Octa OIDC authentication! Downgraded to .net8 as 9 is in preview and has issues with MSBuild.

--- JobTracker/Program.cs
+++ JobTracker/Program.cs
@@ -9,5 +9,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<JobService>();
-
+builder.Services.AddOidcAuthentication(options =>
+{
+    options.ProviderOptions.Authority = "https://dev-b87msvogmhqskscw.uk.auth0.com";
+    options.ProviderOptions.ClientId = "eZLDTyqZTASKqByAykURyqUtbxTYcv1S";
+    options.ProviderOptions.ResponseType = "code";
+    options.ProviderOptions.DefaultScopes.Add("openid");
+    options.ProviderOptions.DefaultScopes.Add("profile");
+    options.ProviderOptions.DefaultScopes.Add("email");
+});
 await builder.Build().RunAsync();
